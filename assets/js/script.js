2d10ea005c3641fbf3de008bf458c07e0dbd44e3
rednotsus <Jiayangc1@gmail.com>
2024-07-13 21:51:54 -0500

    chore: Bump Script Versions, New analytics

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,48 +9,15 @@ function script(text) {
 // ====================================
 // SCRIPT INJECTION
 // ====================================
-fetch("/assets/ads.json")
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.domains.includes(window.location.hostname)) {
-      const adscipterz92 = document.createElement("script");
-      adscipterz92.setAttribute("async", "");
-      adscipterz92.setAttribute(
-        "src",
-        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
-      );
-      adscipterz92.setAttribute("crossorigin", "anonymous");
-      document.head.append(adscipterz92);
-      script("Injected script 1/3 (Ad stuff)");
-    }
-  });
-const gascript = document.createElement("script");
-gascript.setAttribute("async", "");
-gascript.setAttribute(
-  "src",
-  "https://www.googletagmanager.com/gtag/js?id=G-N0LG27M8L8"
-);
-const inlinegascript = document.createElement("script");
-inlinegascript.innerHTML = `window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'G-N0LG27M8L8');`;
-document.head.append(gascript, inlinegascript);
-script("Injected script 2/3");
+const newScript = document.createElement("script");
+newScript.setAttribute("defer", "");
+newScript.setAttribute("data-domain", window.location);
+newScript.setAttribute("src", "https://data.ch3n.cc/js/script.js");
+document.head.append(newScript);
+script("Injected script 1/3");
 
-(function (ovu) {
-  var d = document,
-    s = d.createElement("script"),
-    l = d.scripts[d.scripts.length - 1];
-  s.settings = ovu || {};
-  s.src =
-    "//frighteningship.com/akW.5/wFYNW/dAlmQu2d9okIZZTM9Y6-bS2/5jleSeWvQR9iNnTqEFwGMoDjcyxANYiU0V1/MuTQA/whNJzxEu3Y";
-  s.async = true;
-  l.parentNode.insertBefore(s, l);
-})({});
-script("Injected script 3/3 (More Ad stuff)");
+script("Injected script 2/3 (More Ad stuff)");
 
-// Collect Tab Cloak data from local storage
 var tab = localStorage.getItem("tab");
 if (tab) {
   try {
@@ -77,5 +44,34 @@ document.addEventListener("DOMContentLoaded", function () {
     } else {
       console.warn('No link element with rel="icon" found');
     }
+  }
+});
+
+fetch("/assets/json/ads.json")
+  .then((response) => response.json())
+  .then((data) => {
+    if (data.domains.includes(window.location.hostname)) {
+      const adscipterz92 = document.createElement("script");
+      adscipterz92.setAttribute("async", "");
+      adscipterz92.setAttribute(
+        "src",
+        "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6700774525685317"
+      );
+      adscipterz92.setAttribute("crossorigin", "anonymous");
+      document.head.append(adscipterz92);
+      script("Injected script 3/3 (Adsense)");
+    } else {
+      console.log("Skipping Adsense Injection for this domain.");
+    }
+  });
+
+// key
+var panicKey = localStorage.getItem("panicKey") || "`";
+var panicLink =
+  localStorage.getItem("PanicLink") || "https://canvas.houstonisd.org/";
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === panicKey) {
+    window.location.href = panicLink;
   }
 });
